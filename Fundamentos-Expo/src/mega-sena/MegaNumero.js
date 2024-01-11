e84fa552dabe2f9a2b540cec9688e870bfa600e9
gabrielvedova <bielvedova@gmail.com>
2024-01-11 20:48:21 -0300

    Mini Projeto Mega-Sena

---
+++ Fundamentos-Expo/src/mega-sena/MegaNumero.js
@@ -0,0 +1,27 @@
+﻿import React from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+import estilo from '../estilo'
+
+
+export default ({ num }) => {
+    return (
+        <View style={style.container}>
+            <Text style={[estilo.TxtM, style.Num]}>
+                {num}
+            </Text>
+        </View>
+    )
+}
+
+const  style = StyleSheet.create({
+    container: {
+        backgroundColor: '#000',
+        height: 50,
+        width: 50,
+        margin: 5,
+        borderRadius: 25,
+    },
+    Num: {
+        color: '#FFF',
+    }
+})
