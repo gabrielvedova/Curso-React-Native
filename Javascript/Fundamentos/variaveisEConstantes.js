1a6b9e161a6985247360533c275bc59f38d08198
gabrielvedova <bielvedova@gmail.com>
2024-02-10 20:05:01 -0300

    Revisão de Javascript

---
+++ Javascript/Fundamentos/variaveisEConstantes.js
@@ -0,0 +1,20 @@
+﻿var a = 30
+let b = 40
+
+// Alterar valor da variável
+var a = 40
+b = 15
+
+console.log(a, b)
+
+// Alterar valor da variável 2° forma
+a = 300
+b = 400
+
+console.log(a, b)
+
+// const não se modifica o valor da variável
+const c = 2
+//c = 50
+console.log(c)
+
