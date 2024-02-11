e17103358c86e556e832d50e8a67cd49fdc343c0
gabrielvedova <bielvedova@gmail.com>
2024-02-11 19:39:56 -0300

    Finalizando Fundamentos

---
+++ Javascript/Fundamentos/destructuring2.js
@@ -0,0 +1,11 @@
+﻿const [a] = [10]
+console.log(a)
+
+const [n1, n2, n3, n4, n5, n6 = 0] = [1, 2, 3, 4, 5]
+console.log(n1, n2, n3, n4, n5, n6)
+
+const lista = [n1, n2, n3, n4, n5, n6]
+console.log(lista)
+
+const [, [, nota]] = [[, 8, 8], [9, 6, 8]]
+console.log(nota)
