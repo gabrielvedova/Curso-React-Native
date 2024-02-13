5e110fd6b7073ff9e5ea08dd50723ff792c75f05
gabrielvedova <bielvedova@gmail.com>
2024-02-13 19:05:54 -0300

    Adicionando Função

---
+++ Javascript/Funcao/thisEBind1.js
@@ -0,0 +1,15 @@
+﻿const pessoa = {
+    saudacao: 'Bom dia',
+    falar() {
+        console.log(this.saudacao)
+    }
+}
+
+pessoa.falar()
+
+const falar = pessoa.falar
+falar() //conflito entre paradigmas: funcional e OO
+
+// Solução do conflito entre os paradigmas: bind
+const falarDePessoa = pessoa.falar.bind(pessoa)
+falarDePessoa()
