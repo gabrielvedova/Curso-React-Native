30d11d18b51ed87f4ae66b1ed2290fef768a4aed
gabrielvedova <bielvedova@gmail.com>
2024-02-15 20:09:20 -0300

    Terminando funções arrows e anônimas

--- Javascript/Funcao/arrowFunction3.js
+++ Javascript/Funcao/arrowFunction3.js
@@ -3,3 +3,17 @@
 }
 
 comparaComThis(global)
+
+const obj = {}
+comparaComThis = comparaComThis.bind(obj)
+
+comparaComThis(global)
+comparaComThis(obj)
+
+let comparaComThisArrow = param => console.log(this === param)
+comparaComThisArrow(global)
+comparaComThisArrow(module.exports)
+
+comparaComThisArrow = comparaComThisArrow.bind(obj)
+comparaComThisArrow(global)
+comparaComThisArrow(module.exports)
---
+++ Javascript/Funcao/funcoesAnonimas.js
@@ -0,0 +1,23 @@
+﻿const soma = function (x, y) {
+    return x + y
+}
+
+const imprimirResultado = function (a,  b, operacao = soma) {
+    console.log(operacao(a, b))
+}
+
+imprimirResultado(3, 7)
+imprimirResultado(3, 7, soma)
+imprimirResultado(3, 7, function (x, y) {
+    return x - y
+})
+
+imprimirResultado(3, 7, (x, y) => x * y)
+
+const pessoa = {
+    falar: function () {
+        console.log('Opa')
+    }
+}
+
+pessoa.falar()
