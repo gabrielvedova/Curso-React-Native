655eae5d96efe47b066c7e9e83047114d4418d9f
gabrielvedova <bielvedova@gmail.com>
2024-07-26 15:43:42 -0300

    Inserindo rncrud no Github

---
+++ rncrud/src/data/users.js
@@ -0,0 +1,26 @@
+﻿export default [
+    {
+        id: 1,
+        name: 'João Silva',
+        email: 'josil@gmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2024/02/21/11/34/ai-generated-8587505_960_720.png'
+    },
+    {
+        id: 2,
+        name: 'Mariana Aguiar',
+        email: 'riamasil@gmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2023/10/25/21/04/mouse-8341314_960_720.png'
+    },
+    {
+        id: 3,
+        name: 'Julio Fragoso',
+        email: 'juliafragosa@gmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_960_720.png'
+    },
+    {
+        id: 4,
+        name: 'Rafael Monteiro',
+        email: 'monteiemrafael@gmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2024/05/13/19/19/ufo-8759621_960_720.png'
+    }
+]
---
+++ rncrud/src/view/UserForm.js
@@ -0,0 +1,57 @@
+﻿import React, { useContext, useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import UsersContext from "../context/UsersContext";
+
+export default props => {
+    const [user, setUser] = useState(props.route.params ? props.route.params : {})
+    const { dispatch } = useContext(UsersContext)
+
+    return (
+        <View style={style.form}>
+            <Text>Name</Text>
+            <TextInput
+                style={style.input} 
+                onChangeText={name => setUser({...user, name})}
+                placeholder="Informe o Nome"
+                value={user.name}
+            />
+            <Text>Email</Text>
+            <TextInput
+                style={style.input} 
+                onChangeText={email => setUser({...user, email})}
+                placeholder="Informe o Email"
+                value={user.email}
+            />
+            <Text>URL do Avatar</Text>
+            <TextInput
+                style={style.input} 
+                onChangeText={avatarUrl => setUser({...user, avatarUrl})}
+                placeholder="Informe o Email"
+                value={user.avatarUrl}
+            />
+            <Button 
+                title="Salvar"
+                onPress={() => {
+                    dispatch({
+                        type: user.id ? 'updateUser' : 'createUser',
+                        payload: user
+                    })
+                    props.navigation.goBack()
+                }}
+            />
+        </View>
+    )
+}
+
+const style = StyleSheet.create({
+    form: {
+        padding: 12
+    },
+
+    input: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 10
+    }
+})
