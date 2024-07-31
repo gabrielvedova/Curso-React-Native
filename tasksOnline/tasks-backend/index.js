553089fe5377b0bfdb796a4157ede2f98faeed59
gabrielvedova <bielvedova@gmail.com>
2024-07-31 18:52:02 -0300

    Criando o backend

---
+++ tasksOnline/tasks-backend/index.js
@@ -0,0 +1,11 @@
+﻿const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.status(200).send("Meu Backend!!!")
+})
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Backend executando na porta ${PORT}...`)
+})
